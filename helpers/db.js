46722e473b11027068ce0f83129809ab9c0b7ee6
19khaled88 <kkhaled88hasan@gmail.com>
2024-03-28 15:33:23 +0600

    added home page icons and navigation buttons

---
+++ helpers/db.js
@@ -0,0 +1,12 @@
+export const homepageImage = {
+  images: [
+    "./images/Amy Logo.png",
+    "./images/Be Rich Logo.png",
+    "./images/Be Rich Manufacturing Logo.png",
+    "./images/BeFresh Logo.png",
+    "./images/BeFreshEdu Logo.png",
+    "./images/CtgShop.com_Logo,_2018.svg.png",
+    "./images/South East Money Exchange Logo.png",
+    "./images/x-limited.png",
+  ],
+};
---
+++ js/homepage.js
@@ -0,0 +1,34 @@
+import { homepageImage } from "../helpers/db.js";
+
+function mapHomepageIcon() {
+
+  const container = document.getElementById("main");// Get the image element by its id
+
+  container.innerHTML = "";
+
+  homepageImage.images.map((file) => {
+    
+    const imageElement = document.createElement("img");// Create an img element for each file
+    
+    imageElement.src = file;// Set the src attribute to the URL of the image file
+
+    
+    imageElement.alt = "Description of the image";// Set the alt attribute
+
+    
+    imageElement.width = 200;// Set the width and height attributes
+    imageElement.height = 100;// Set the width and height attributes
+
+    // link.href = file;
+    // link.textContent = file;
+    // link.target = '_blank'; // Open in a new tab
+
+    
+
+    // Append the img elements to the container
+    container.appendChild(imageElement);
+    
+  });
+}
+
+mapHomepageIcon();
