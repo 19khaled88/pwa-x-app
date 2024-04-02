98ef0d2bfd5902818ec09bfbd32b0418a7ae99b9
19khaled88 <kkhaled88hasan@gmail.com>
2024-04-02 15:17:08 +0600

    modified es6 import method to fetch api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,42 +1,41 @@
-function routeToPage(path,data) {
-  window.location.href = path;
- 
-} 
+// import { homepageImage } from "../../helpers/db.js";
 
-import { homepageImage } from "../../helpers/db.js";
+async function fetchData() {
+  try {
+    const response = await fetch("./helpers/dbs.json");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.text();
+    const parsedData = JSON.parse(data);
+    const container = document.getElementById("main"); // Get the image element by its id
 
+    container.innerHTML = "";
+    parsedData.Image.map((file,index) => {
+      const div = document.createElement("div"); // Create a div element
+      const itemBtn = document.createElement("button");
+      const imageElement = document.createElement("img"); // Create an img element for each file
+      imageElement.src = file; // Set the src attribute to the URL of the image file
+      imageElement.alt = "No image"; // Set the alt attribute
+      imageElement.width = 200; // Set the width and height attributes
+      imageElement.height = 100; // Set the width and height attributes
+      itemBtn.type = "button";
+      itemBtn.classList.add("item-add-btn");
 
+      if ((index + 1) % 2 === 0) {
+        div.classList.add("even-div");
+      } else {
+        div.classList.add("odd-div");
+      }
 
-function mapHomepageIcon() {
-
-  const container = document.getElementById("main");// Get the image element by its id
-
-  container.innerHTML = "";
-
-  homepageImage.images.map((file) => {
-    
-    const imageElement = document.createElement("img");// Create an img element for each file
-    
-    imageElement.src = file;// Set the src attribute to the URL of the image file
-
-    
-    imageElement.alt = "Description of the image";// Set the alt attribute
-
-    
-    imageElement.width = 200;// Set the width and height attributes
-    imageElement.height = 100;// Set the width and height attributes
-
-    // link.href = file;
-    // link.textContent = file;
-    // link.target = '_blank'; // Open in a new tab
-
-    // Append the img elements to the container
-    container.appendChild(imageElement);
-    
-  });
+      
+      div.appendChild(itemBtn);
+      itemBtn.appendChild(imageElement);
+      container.appendChild(div);
+    });
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
 }
 
-
-
-mapHomepageIcon();
-
+fetchData();
